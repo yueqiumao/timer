f64bf5e47d3441cf0812353a66614eb8133fa5b7
yueqiumao <u2nn@qq.com>
2017-07-23 21:04:52 +0800

    加入按钮控制

--- timer.ino
+++ timer.ino
@@ -10,10 +10,18 @@
     9          WR
 */
 
+#include "ets_sys.h"
+#include "osapi.h"
+#include "os_type.h"
+#include "user_interface.h"
+#include "eagle_soc.h"
+
 long timestamp = 0;
+os_timer_t keyTimer;
+bool pressed = false;
 
 void drawChar(int pos, char ch) {
-    digitalWrite(14, HIGH);
+    digitalWrite(10, HIGH);
     delay(1);
 
     digitalWrite(12, (pos&1) ? HIGH : LOW );
@@ -29,10 +37,10 @@ void drawChar(int pos, char ch) {
     digitalWrite(15, (ch&(1<<6) ? HIGH : LOW ));
     delay(1);
 
-    digitalWrite(14, LOW );
+    digitalWrite(10, LOW );
     delay(1);
 
-    digitalWrite(14, HIGH);
+    digitalWrite(10, HIGH);
     delay(1);
 }
 
@@ -47,6 +55,27 @@ void flashScreenOnce() {
     delay(500);
 }
 
+void intrKeyPress() {
+    if (!pressed) {
+        pressed = true;
+        os_timer_disarm(&keyTimer);
+        os_timer_setfn(&keyTimer, (os_timer_func_t*)pressHandler, NULL);
+        os_timer_arm(&keyTimer, 50, 0);
+    }
+}
+
+void pressHandler() {
+    pressed = false;
+    if (digitalRead(14) == LOW) {
+        resetTimer();
+    }
+}
+
+void resetTimer() {
+    timestamp = millis() + 25l*60*1000;
+    // timestamp = 0;
+}
+
 void setup() {
     pinMode(0, OUTPUT);
     pinMode(1, OUTPUT);
@@ -57,21 +86,28 @@ void setup() {
     pinMode(15, OUTPUT);
     pinMode(12, OUTPUT);
     pinMode(13, OUTPUT);
-    pinMode(14, OUTPUT);
+    pinMode(10, OUTPUT);
 
-    timestamp = millis() + 25l*60*1000;
+    pinMode(14, INPUT_PULLUP);
+    attachInterrupt(14, intrKeyPress, FALLING);
+
+    resetTimer();
+    Serial.begin(74880);
 }
 
 void loop() {
-    if (timestamp >= millis()) {
+    if (timestamp + (5 * 60 * 1000) >= millis()) {
         long interval = (timestamp - millis()) / 1000;
-        int minute = interval / 60;
-        int second = interval % 60;
+        // int minute = interval / 60;
+        // int second = interval % 60;
 
-        drawChar(3, '0' + (minute / 10 % 10));
-        drawChar(2, '0' + (minute % 10));
-        drawChar(1, '0' + (second / 10 % 10));
-        drawChar(0, '0' + (second % 10));
+        // drawChar(3, '0' + (minute / 10 % 10));
+        // drawChar(2, '0' + (minute % 10));
+        // drawChar(1, '0' + (second / 10 % 10));
+        // drawChar(0, '0' + (second % 10));
+        char buff[32];
+        sprintf(buff, "%d", interval);
+        Serial.println(buff);
     } else {
         flashScreenOnce();
     }
